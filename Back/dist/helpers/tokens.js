ef9c6ca813b633f97ce21bd3daa248ad695fb798
Matthew Cook <cookm353@gmail.com>
2023-06-14 13:40:39 -0400

    transpiled TS file

--- Back/dist/helpers/tokens.js
+++ Back/dist/helpers/tokens.js
@@ -1,2 +1,12 @@
-const createToken = "";
-module.exports = createToken;
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require("../config");
+const createToken = (user) => {
+    // console.assert(user.isAdmin !== undefined,
+    //     "createToken passed user without isAdmin property")
+    let payload = {
+        username: user.username,
+        isAdmin: user.isAdmin || false
+    };
+    return jwt.sign(payload, SECRET_KEY);
+};
+module.exports = { createToken };
