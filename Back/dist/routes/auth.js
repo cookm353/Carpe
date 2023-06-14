6050dac67586e844ce5b64028e2796a7a911093e
Matthew Cook <cookm353@gmail.com>
2023-06-14 13:40:08 -0400

    created script

---
+++ Back/dist/routes/auth.js
@@ -0,0 +1,13 @@
+/* Authentication related routes */
+const jsonschema = require("jesonschema");
+const express = require("express");
+const User = require("../models/users");
+const { createToken } = require("../helpers/tokens");
+const router = new express.Router();
+/** POST /auth/token: { username, password } => { token }
+ *
+ * Returns JWT token used for auth w/ other requests
+ *
+ * Auth needed: none
+*/
+router.post("/token");
