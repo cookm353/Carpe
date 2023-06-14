d5cca8a5a0a6c591b54ee7d40aaaa346505f7622
Matthew Cook <cookm353@gmail.com>
2023-06-14 13:39:00 -0400

    added imports

--- Back/__tests__/entry.test.js
+++ Back/__tests__/entry.test.js
@@ -1,3 +1,10 @@
+process.env.NODE_ENV = "test"
+
+const request = require("supertest")
+const db = require("../dist/db")
+const Entry = require("../dist/models/entry")
+const User = require("../dist/models/users")
+
 test("", () => {
     expect(6).toEqual(6)
 })
