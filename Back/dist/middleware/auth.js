910d9068829a8ebda9ff1f61427485da2f168619
Matthew Cook <cookm353@gmail.com>
2023-06-14 19:34:12 -0400

    added docstring for first function

--- Back/dist/middleware/auth.js
+++ Back/dist/middleware/auth.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require('../expressError');
+/** Middleware for authenticating users
+ *
+ * Verify a token if provided.  If token is valid, store payload
+ * on res.locals
+ *
+ * Not providing a token or having an invalid token isn't an error
+*/
 function authenticateJWT(req, resp, next) {
     try {
         const authHeader = req.headers && req.headers.authorization;
