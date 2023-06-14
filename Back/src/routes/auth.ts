473aee03b0919f196b6c43c120fe3f28a4daf413
Matthew Cook <cookm353@gmail.com>
2023-06-14 14:08:33 -0400

    imported schemas, added token and register routes

--- Back/src/routes/auth.ts
+++ Back/src/routes/auth.ts
@@ -1,10 +1,13 @@
 /* Authentication related routes */
 
-const jsonschema = require("jesonschema")
+const jsonschema = require("jsonschema")
 const express = require("express")
 
 const User = require("../models/users")
 const { createToken } = require("../helpers/tokens")
+const userAuthSchema = require("../../schemas/userAuth.json")
+const userRegisterSchema = require("../../schemas/userRegister.json")
+const { BadRequestError } = require("../expressError")
 
 const router = new express.Router()
 
@@ -15,4 +18,50 @@ const router = new express.Router()
  * Auth needed: none
 */
 
-router.post("/token")
+router.post("/token", async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, userAuthSchema)
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack)
+            throw new BadRequestError(errs)
+        }
+
+        const { username, password } = req.body
+        const user = await User.authenticate(username, password)
+        const token = createToken(user)
+
+        return res.json({ token })
+    } catch (err) {
+        return next(err)
+    }
+})
+
+/** POST /auth/register: { user } => { token } 
+ * 
+ * User must include { username, password, firstName, email}
+ * 
+ * Returns JWT that can be to authenticate other requests
+ * 
+ * Auth required: none
+*/
+
+router.post("/register", async (req, res, next) => {
+    console.log("foo")
+    try {
+        const validator = jsonschema.validate(req.body, userRegisterSchema)
+
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack)
+            throw new BadRequestError(errs)
+        }
+
+        const newUser = await User.register({ ...req.body, isAdmin: false })
+        const token = createToken(newUser)
+        return res.status(201).json({ token })
+
+    } catch (err) {
+        return next(err)
+    }
+})
+
+module.exports = router
