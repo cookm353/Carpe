50bd749a7ea45fc380752cc4572671cc8c3d98f6
Matthew Cook <cookm353@gmail.com>
2023-08-25 16:37:39 -0400

    created

---
+++ Back/carpe-seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO users( username, password, first_name, email, is_admin, get_emails)
+VALUES (
+    "alice",
+    "test123",
+    "Alice",
+    "alice@test.com",
+    FALSE,
+    TRUE
+), 
+(
+    "bob",
+    "test123",
+    "Bob",
+    "bob@test.com",
+    FALSE,
+    FALSE
+)
+
