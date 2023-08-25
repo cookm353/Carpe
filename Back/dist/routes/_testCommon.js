44ffc22b5fd28d6d5d309e7eefd9bd00c5f21331
Matthew Cook <cookm353@gmail.com>
2023-08-25 16:38:12 -0400

    created

---
+++ Back/dist/routes/_testCommon.js
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = "test";
+const bcrypt = require("bcrypt");
+const db = require('../db');
+const User = require("../models/users");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+async function commonBeforeAll() {
+    // await db.query('DELETE FROM entries')
+    await db.query('DELETE FROM users');
+    await User.register({
+        username: "tom",
+        password: "test123",
+        email: "tom@test.com",
+        firstName: "Tom",
+        isAdmin: true
+    });
+}
+async function commonBeforeEach() {
+    await db.query("BEGIN");
+}
+async function commonAfterEach() {
+    await db.query("ROLLBACK");
+}
+async function commonAfterAll() {
+    await db.end();
+}
+module.exports = {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterAll,
+    commonAfterEach
+};
