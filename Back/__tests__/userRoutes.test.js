dd2ae73146c0b31c302a2b58667cc3a1ed71b539
Matthew Cook <cookm353@gmail.com>
2023-08-26 14:47:16 -0400

    + test tokens, + GET /user and /user/username

--- Back/__tests__/userRoutes.test.js
+++ Back/__tests__/userRoutes.test.js
@@ -10,8 +10,12 @@ const {
     commonBeforeAll,
     commonBeforeEach,
     commonAfterAll,
-    commonAfterEach
+    commonAfterEach,
+    adminJWT,
+    userJWT
 } = require('../dist/routes/_testCommon')
+const { UnauthorizedError } = require('../dist/expressError')
+const e = require("express")
 
 beforeAll(commonBeforeAll)
 beforeEach(commonBeforeEach)
@@ -28,12 +32,75 @@ describe("POST /user", () => {
 
 /* GET /user */
 
+describe("GET /user", () => {
+    it("throws error for non-admins", async () => {
+        const resp = await request(app).get('/user')
+
+        expect(resp.statusCode).toBe(401)
+        expect(resp.body.error.message).toBe("Unauthorized")
+    })
+
+    it("retrieves user info for admin", async () => {
+        const resp = await request(app)
+        .get("/user")
+        .set("authorization", `Bearer ${adminJWT}`)
+
+        expect(resp.statusCode).toBe(200)
+    })
+})
 
 
 /* GET /user/:username */
 
+describe("GET /user/:username", () => {
+    it("throws error with no JWT", async () => {
+        const resp = await request(app).get("/user/tom")
+        expect(resp.statusCode).toBe(401)
+    })
+
+    // it("throws error with invalid JWT", async () => {
+
+    // })
+
+    // it("throws error for wrong user", async () => {
+
+    // })
+
+    it("works for right user", async () => {
+        const resp = await request(app)
+            .get('/user/chuck')
+            .set('authorization', `Bearer ${userJWT}`)
+        
+        expect(resp.statusCode).toBe(200)
+        expect(resp.body).toEqual({
+            "user": {
+                email: "chuck@test.com",
+                firstName: "Chuck",
+                isAdmin: false,
+                username: "chuck"
+            }
+        })
+
+    })
+
+    it("works for admin on other user", async () => {
+        const resp = await request(app)
+            .get("/user/tom")
+            .set('authorization', `Bearer ${adminJWT}`)
+
+        expect(resp).toEqual("")
+        expect(resp.statusCode).toBe(200)
+    })
+})
 
 /* PATCH /user/:username */
 
+describe("PATCH /user/:username", () => {
+
+})
 
 /* DELETE /user/:username */
+
+describe("DELETE /user/:username", () => {
+
+})
