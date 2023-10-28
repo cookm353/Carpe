185f564eab09aa2d68965f683d6fccf66800923b
Matthew Cook <cookm353@gmail.com>
2023-10-28 18:12:52 -0400

    added link for new entry, cleaned up appearance

--- Front/src/Navbar.jsx
+++ Front/src/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NewEntryForm from "./NewEntryForm";
 // import { faUser } from '@fortawesome/free-solid-svg-icons'
 // import RegisterForm from "./RegisterForm";
 
 
-const Navbar = ({username, setUsername, setToken}) => {
+
+
+const Navbar = ({username, token, setUsername, setToken}) => {
     const handleLogout = () => {
         setToken("")
         setUsername("")
@@ -14,35 +17,44 @@ const Navbar = ({username, setUsername, setToken}) => {
     return (
 
         <>
-            <nav className="navbar d-flex">
-                <div className="row">
-                    <div className="col-2">
-                        <NavLink className="link" to="/">
-                            Carpe
-                        </NavLink>
+            <div className="row d-flex justify-content-between">
+                <div className="col-2">
+                    <div className="row">
+                        <div className="col">
+                            <NavLink className="link" to="/">
+                                Carpe
+                            </NavLink>
+                        </div>
+                        {token ?
+                        <div className="col">
+                            <NavLink className="link" to="/newEntry">
+                                New Entry
+                            </NavLink>
+                        </div> :
+                        <></>
+                        }
                     </div>
-                    <div className="col-8" id="navbarSpacer"></div>
                 </div>
-                    {username !== "" ?
-                    <div className="col-2 d-flex justify-content-between">
-                        <NavLink className="link" to="/:username">
-                            <FontAwesomeIcon icon="faUser" />
-                            {username}
-                        </NavLink>
-                        <NavLink onClick={handleLogout} className="link" to="/">
-                            Log out
-                        </NavLink>
-                    </div> :
-                    <div className="col-2 d-flex justify-content-between">
-                        <NavLink className="link" to="/register">
-                            Register
-                        </NavLink>
-                        <NavLink className="link" to="/login">
-                            Login
-                        </NavLink>
-                    </div>            
-                    }
-            </nav>
+                {username !== "" ?
+                <div className="col-2 d-flex justify-content-between">
+                    <NavLink className="link" to={`/${username}`}>
+                        <FontAwesomeIcon icon="faUser" />
+                        {username}
+                    </NavLink>
+                    <NavLink onClick={handleLogout} className="link" to="/">
+                        Log out
+                    </NavLink>
+                </div> :
+                <div className="col-2 d-flex justify-content-between">
+                    <NavLink className="link" to="/register">
+                        Register
+                    </NavLink>
+                    <NavLink className="link" to="/login">
+                        Login
+                    </NavLink>
+                </div>            
+                }
+            </div>
         </>
     )
 }
