8e3d53e2ae5c300c2aec943fc4d039114c54e751
Matthew Cook <cookm353@gmail.com>
2023-10-29 14:30:38 -0400

    added props to routes, added entry accordion route

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -24,22 +24,21 @@ const App = () => {
   
 	return (
 		<>
-		{/* Open Unit41 for help on React Router! */}
 			<BrowserRouter>
 				<Navbar username={username} token={token} setUsername={setUsername} setToken={setToken} className="my-1"/>
 				<Routes>
 					<Route exact path="/" element={<Home token={token}/>}/>
 					<Route exact path="/login" element={<LoginForm setToken={setToken} setUsername={setUsername}/>}/>
-					<Route exact path="/register" element={<RegisterForm/>}/>
-					<Route exact path="/newEntry" element={<NewEntryForm token={token}/>}/>
-					{/* <Route exact path="/newEntry" element={<NewEntryForm/>}/> */}
-					<Route exact path="/entries" element={<DateAccordion/>}/>
+					<Route exact path="/register" element={<RegisterForm token={token} username={username}/>}/>
+					<Route exact path="/newEntry" element={<NewEntryForm token={token} username={username}/>}/>
+					<Route exact path="/entries" element={<DateAccordion username={username} token={token}/>}/>
 					<Route exact path="/:username" element={<Home/>}/>
 				</Routes>
 			</BrowserRouter>
 			{
 				token ?
 				<h1>{username}</h1> :
+				// <DateAccordion token={token} username={username}/> :
 				<h1>{"Nope"}</h1>
 			}
 		</>
